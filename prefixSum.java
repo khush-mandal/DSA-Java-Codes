6c07e5b044545c9bdcbc57f8bac6e59e67b6e2cb
khush mandal <khush09mandal@gmail.com>
2026-09-04 18:57:07 +0530

    solution of finding maximum subarray sum using prefixsum array

--- prefixSum.java
+++ prefixSum.java
@@ -1,17 +1,22 @@
 public class prefixSum {
     public static void main (String args []){
-        int array[] = {2,4,6,8,9};
+        int array[] = {2,4,6,8,9 };
         int prefixSum [] = new int[array.length];
         int currSum ;
-        for (int i=0;i<array.length;i++){
+        int maxSum = Integer.MIN_VALUE;
+        prefixSum[0]=array[0];
+        for(int i=1; i<prefixSum.length;i++){
+            prefixSum[i] = prefixSum[i-1] + array[i];
+        }  
+        for (int i=0;i<=array.length;i++){
             for (int j=i;j<array.length;j++){
-                currSum=0;
-                for (int k=i;k<j;k++){
-                    currSum += array[k];
-                    prefixSum[i]=currSum;
+                 currSum=0;
+              currSum = i==0 ? prefixSum[j] : prefixSum[j] - prefixSum[i-1]; 
+              if(currSum > maxSum){
+                maxSum = currSum;
+              }
         }
-        System.out.println(prefixSum);
         }
-        }
+        System.out.println("max sum is : "+ maxSum);
     }
 }
